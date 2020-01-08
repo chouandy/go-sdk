eb05dd50a88eebe00cd7c3189dbfa35479596df3
ChouAndy <chouandy625@gmail.com>
2020-01-08 22:37:03 +0800

    Add http auth package RevokeAccessTokenHandler
    Signed-off-by: ChouAndy <chouandy625@gmail.com>

--- http/auth/http.go
+++ http/auth/http.go
@@ -14,3 +14,17 @@ func GetAccessToken(c *gin.Context) string {
 
 	return accessToken
 }
+
+// RevokeAccessTokenHandler revoke access token handler
+func RevokeAccessTokenHandler(c *gin.Context) {
+	// Get access token
+	accessToken := GetAccessToken(c)
+
+	// Revoke access token
+	if len(accessToken) > 0 {
+		userID, err := GetUserIDByAccessToken(accessToken)
+		if err == nil {
+			RevokeAccessToken(accessToken, userID)
+		}
+	}
+}
