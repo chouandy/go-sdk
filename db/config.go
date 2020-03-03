c44573fe9b9c51fd5adf54eb7bc9e9757f1da443
ChouAndy <chouandy625@gmail.com>
2020-03-03 18:27:04 +0800

    Update db config for postgres
    Signed-off-by: ChouAndy <chouandy625@gmail.com>

--- db/config.go
+++ db/config.go
@@ -3,6 +3,7 @@ package db
 import (
 	"errors"
 	"fmt"
+	"net/url"
 	"os"
 	"strconv"
 
@@ -10,13 +11,34 @@ import (
 )
 
 // DataSourceFormat data source format
-var DataSourceFormat = "%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true"
+var DataSourceFormat = map[string]string{
+	"mysql":    "%s:%s@tcp(%s:%s)/%s?%s",
+	"postgres": "postgres://%s:%s@%s:%s/%s?%s",
+}
 
 // DataSourceWithoutDatabaseFormat data source without database format
-var DataSourceWithoutDatabaseFormat = "%s:%s@tcp(%s:%s)/?charset=%s"
+var DataSourceWithoutDatabaseFormat = map[string]string{
+	"mysql":    "%s:%s@tcp(%s:%s)/?%s",
+	"postgres": "postgres://%s:%s@%s:%s/?%s",
+}
 
-// DatabaseURLFormat database url format
-var DatabaseURLFormat = "%s://%s:%s@tcp(%s:%s)/%s?charset=%s"
+// CreateDatabaseStatementFormat create database statement format
+var CreateDatabaseStatementFormat = map[string]string{
+	"mysql":    "CREATE DATABASE `%s` DEFAULT CHARACTER SET = '%s' DEFAULT COLLATE '%s';",
+	"postgres": `CREATE DATABASE "%s" ENCODING = '%s' LC_COLLATE = '%s' LC_CTYPE = '%s';`,
+}
+
+// DropDatabaseStatementFormat drop database statement
+var DropDatabaseStatementFormat = map[string]string{
+	"mysql":    "DROP DATABASE `%s`",
+	"postgres": `DROP DATABASE "%s"`,
+}
+
+// DatabaseURLFormat data source format
+var DatabaseURLFormat = map[string]string{
+	"mysql":    "mysql://%s:%s@tcp(%s:%s)/%s?%s",
+	"postgres": "postgres://%s:%s@%s:%s/%s?%s",
+}
 
 // Config config struct
 type Config struct {
@@ -31,6 +53,7 @@ type Config struct {
 	MaxOpenConns   int
 	MaxIdleConns   int
 	LogMode        bool
+	SSLMode        string
 }
 
 // NewConfig new config
@@ -45,6 +68,7 @@ func NewConfig() (*Config, error) {
 		Password:       os.Getenv("DB_PASSWORD"),
 		Charset:        os.Getenv("DB_CHARSET"),
 		DefaultCollate: os.Getenv("DB_DEFAULT_COLLATE"),
+		SSLMode:        os.Getenv("DB_SSL_MODE"),
 	}
 	// Validate driver
 	if err := config.Validate(); err != nil {
@@ -100,18 +124,39 @@ func (c *Config) GetLogModeFromEnv() {
 
 // LoadDefault load default
 func (c *Config) LoadDefault() {
-	// Set port default value
-	if len(c.Port) == 0 {
-		c.Port = "3306"
+	// Postgres default values
+	if c.Driver == "postgres" {
+		// Set port default value
+		if len(c.Port) == 0 {
+			c.Port = "5432"
+		}
+		// Set charset default value
+		if len(c.Charset) == 0 {
+			c.Charset = "UTF8"
+		}
+		// Set default collate default value
+		if len(c.DefaultCollate) == 0 {
+			c.DefaultCollate = "en_US.utf8"
+		}
+		// Set ssl mode default value
+		if c.SSLMode == "" {
+			c.SSLMode = "disable"
+		}
+	} else {
+		// Set port default value
+		if len(c.Port) == 0 {
+			c.Port = "3306"
+		}
+		// Set charset default value
+		if len(c.Charset) == 0 {
+			c.Charset = "utf8"
+		}
+		// Set default collate default value
+		if len(c.DefaultCollate) == 0 {
+			c.DefaultCollate = "utf8_general_ci"
+		}
 	}
-	// Set charset default value
-	if len(c.Charset) == 0 {
-		c.Charset = "utf8"
-	}
-	// Set default collate default value
-	if len(c.DefaultCollate) == 0 {
-		c.DefaultCollate = "utf8_general_ci"
-	}
+
 	// Set max idle conns default value
 	if c.MaxIdleConns == 0 {
 		c.MaxIdleConns = 30
@@ -124,22 +169,66 @@ func (c *Config) LoadDefault() {
 
 // DataSource data source
 func (c *Config) DataSource() string {
-	return fmt.Sprintf(DataSourceFormat,
-		c.Username, c.Password, c.Host, c.Port, c.Database, c.Charset,
+	// New query
+	query := url.Values{}
+	if c.Driver == "postgres" {
+		query.Set("sslmode", c.SSLMode)
+	} else {
+		query.Set("charset", c.Charset)
+		query.Set("parseTime", "true")
+	}
+
+	return fmt.Sprintf(DataSourceFormat[c.Driver],
+		c.Username, c.Password, c.Host, c.Port, c.Database, query.Encode(),
 	)
 }
 
 // DataSourceWithoutDatabase data source without database
 func (c *Config) DataSourceWithoutDatabase() string {
-	return fmt.Sprintf(DataSourceWithoutDatabaseFormat,
-		c.Username, c.Password, c.Host, c.Port, c.Charset,
+	// New query
+	query := url.Values{}
+	if c.Driver == "postgres" {
+		query.Set("sslmode", c.SSLMode)
+	} else {
+		query.Set("charset", c.Charset)
+	}
+
+	return fmt.Sprintf(DataSourceWithoutDatabaseFormat[c.Driver],
+		c.Username, c.Password, c.Host, c.Port, query.Encode(),
 	)
 }
 
-// DatabaseURL return database url
+// CreateDatabaseStatement create database statement
+func (c *Config) CreateDatabaseStatement() string {
+	// New args
+	var args []interface{}
+	if c.Driver == "postgres" {
+		args = []interface{}{c.Database, c.Charset, c.DefaultCollate, c.DefaultCollate}
+	} else {
+		args = []interface{}{c.Database, c.Charset, c.DefaultCollate}
+	}
+
+	return fmt.Sprintf(CreateDatabaseStatementFormat[c.Driver], args...)
+}
+
+// DropDatabaseStatement drop database statement
+func (c *Config) DropDatabaseStatement() string {
+	return fmt.Sprintf(DropDatabaseStatementFormat[c.Driver], c.Database)
+}
+
+// DatabaseURL database url
 func (c *Config) DatabaseURL() string {
-	return fmt.Sprintf(DatabaseURLFormat,
-		c.Driver, c.Username, c.Password, c.Host, c.Port, c.Database, c.Charset,
+	// New query
+	query := url.Values{}
+	if c.Driver == "postgres" {
+		query.Set("sslmode", c.SSLMode)
+	} else {
+		query.Set("charset", c.Charset)
+		query.Set("parseTime", "true")
+	}
+
+	return fmt.Sprintf(DatabaseURLFormat[c.Driver],
+		c.Username, c.Password, c.Host, c.Port, c.Database, query.Encode(),
 	)
 }
 
