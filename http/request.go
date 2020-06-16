d0775d33929890670c519ea05336134ffec9c363
ChouAndy <chouandy625@gmail.com>
2020-06-16 11:16:18 +0800

    Update http/pagination.go
    Signed-off-by: ChouAndy <chouandy625@gmail.com>

---
+++ http/request.go
@@ -0,0 +1,23 @@
+package http
+
+// ListOptions list options struct
+type ListOptions struct {
+	Page     int64 `form:"page"`
+	PageSize int64 `form:"page_size"`
+	Count    bool  `form:"count"`
+}
+
+// IsPagination is pagination
+func (o *ListOptions) IsPagination() bool {
+	return o.Page > 0 && o.PageSize > 0
+}
+
+// Limit return limit for sql query
+func (o *ListOptions) Limit() int64 {
+	return o.PageSize
+}
+
+// Offset return offset for sql query
+func (o *ListOptions) Offset() int64 {
+	return (o.Page - 1) * o.PageSize
+}
